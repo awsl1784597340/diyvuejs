7f9547f6cf4c118199470e06db7e089d92b38102
awsl1784597340 <grandgadsix@sjtu.edu.cn>
2020-03-21 13:34:40 +0800

    pw

--- src/App.vue
+++ src/App.vue
@@ -9,7 +9,7 @@
     </div>
     <div class="mui-input-row">
         <label>class</label>
-        <input type="password" class="mui-input-password" placeholder="请输入ta的职介">
+        <input type="text" class="mui-input-clear" placeholder="请输入ta的职介">
     </div>
     <div class="mui-button-row">
         <button type="button" class="mui-btn mui-btn-primary" >查询信息</button>
