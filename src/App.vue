e8fbed007fd766facb97d60c640e8a69b6dc1dae
awsl1784597340 <grandgadsix@sjtu.edu.cn>
2020-03-21 13:29:39 +0800

    1

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,34 @@
 <template>
-  <div>
-    <h1>这是 App 组件</h1>
-  </div>
+
+<div class="app-container">
+      <mt-header fixed title="海哥牌fgo英灵查询"></mt-header>
+  <form class="mui-input-group">
+    <div class="mui-input-row">
+        <label>name</label>
+    <input type="text" class="mui-input-clear" placeholder="请输入ta的名字或昵称">
+    </div>
+    <div class="mui-input-row">
+        <label>class</label>
+        <input type="password" class="mui-input-password" placeholder="请输入ta的职介">
+    </div>
+    <div class="mui-button-row">
+        <button type="button" class="mui-btn mui-btn-primary" >查询信息</button>
+        <button type="button" class="mui-btn mui-btn-danger" >查询图片</button>
+    </div>
+</form>
+</div>
+
 </template>
 
 <script>
+
 </script>
 
 
 <style lang="scss" scoped>
-
+.app-container{
+  padding-top: 40px;
+  padding-bottom: 50px;
+  overflow-x:hidden;
+}
 </style>
--- src/main.js
+++ src/main.js
@@ -1,2 +1,30 @@
-// 入口文件
-console.log('OK')
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import VueResource from 'vue-resource'
+Vue.use(VueResource)
+Vue.http.options.root = 'http://127.0.0.1:8000'
+
+Vue.use(VueRouter)
+import router from './router.js'
+
+import app from './App.vue'
+
+import { Header, Swipe, SwipeItem } from 'mint-ui'
+
+import 'mint-ui/lib/style.css'
+
+import './lib/mui/css/mui.min.css'
+
+
+
+Vue.component(Header.name, Header)
+Vue.component(Swipe.name, Swipe)
+Vue.component(SwipeItem.name, SwipeItem)
+
+
+
+var vm = new Vue({
+    el: '#app',
+    render: c => c(app),
+    router
+})
